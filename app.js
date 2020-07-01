81ae7adf68f8ef96c2e9eff836f429d7545636c9
GasÃpar <gaspar.av88@gmail.com>
2020-07-01 16:47:46 -0300

    Creacion pagina ML

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 
-const app = express()
+const app = express();
 
-app.listen(3005, (req, res) => {
-    console.log("Servidor escuchando en el puerto 3005")
-}
+//como no puedo ver las imagenes xq mi computador es Vruto como yo, entonces tengo que hacer app.use//
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+app.listen(3005, () => {
+    console.log("Puerto escuchando en 3005")
+});
