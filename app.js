5ed7d43b66ab01b6a0a41cd4933da6238cf9d2be
GasÃpar <gaspar.av88@gmail.com>
2020-07-14 23:20:47 -0300

    actualizando ejercicio

--- app.js
+++ app.js
@@ -6,10 +6,27 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: false }));
+
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
-})
-
-app.listen(3005, () => {
-    console.log("Puerto escuchando en 3005")
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.post('/', (req, res) => {
+    res.send(req.body);
+});
+
+app.listen(3009, () => {
+    console.log("Puerto escuchando en 3009") 
+});
+
+ 
